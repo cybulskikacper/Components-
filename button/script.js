e10a60084f3fb4fb72c6d91df296c85c38ed66f6
Kacper <kacpercybulski@icloud.com>
2023-05-16 22:40:00 +0200

    working on displaying effects on click

--- button/script.js
+++ button/script.js
@@ -1,1 +1,22 @@
-console.log('Nic tu nie ma 😱')
+const btn = document.querySelector('.btn')
+
+const btnAnimation = e => {
+	const top = e.clientY
+	const left = e.clientX
+	// pozycja w którą klikamy
+
+	const btnTopPosition = e.target.offsetTop
+	const btnLeftPosition = e.target.offsetLeft
+	// pozycja przycisku
+
+	const insideBtnTop = top - btnTopPosition
+	const insideBtnLeft = left - btnLeftPosition
+
+	const circle = document.createElement('span')
+	circle.classList.add('circle')
+    circle.style.top = insideBtnTop
+
+	e.target.appendChild(circle)
+}
+
+btn.addEventListener('click', btnAnimation)
