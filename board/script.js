2c8ebc3ff5f70093e81d81bbf5e2721a7f1d0c46
Kacper <kacpercybulski@icloud.com>
2023-05-21 12:35:57 +0200

    displaying all squares

---
+++ board/script.js
@@ -0,0 +1,22 @@
+const box = document.querySelector('.box')
+const speedBtns = document.querySelectorAll('[data-setting="speed"]')
+const colorBtns = document.querySelectorAll('[data-setting="color"]')
+const slider = document.querySelector('#slider')
+const sliderInfo = document.querySelector('.slider-info')
+
+const squares = 546
+let sliderValue = 70
+let range = 360
+
+const createSquare = () => {
+	for (let i = 0; i < squares; i++) {
+		const square = document.createElement('div')
+		square.classList.add('square')
+
+
+        box.appendChild(square)
+
+	}
+}
+
+createSquare()
