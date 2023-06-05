e2d93bb1b557ceb21ce5af635bede33d5b8676e8
Kacper <kacpercybulski@icloud.com>
2023-06-05 22:46:34 +0200

    downloaded all items needed

---
+++ slider/script.js
@@ -0,0 +1,8 @@
+const sliderBox = document.querySelector('.slieder-box')
+const leftBtn = document.querySelector('.btn-left')
+const rightBtn = document.querySelector('.bth-right')
+const carouseleImages = document.querySelectorAll('.slider-img')
+const carouseleWidth = 800 // odpowiada wartosci w css
+const carouseleSpeed = 3000
+
+let index = 0
