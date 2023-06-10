17debe69b71ceb5ae29385ce56105fb887213eb3
Kacper <kacpercybulski@icloud.com>
2023-06-10 09:40:18 +0200

    app is done

---
+++ presents/script.js
@@ -0,0 +1,46 @@
+const presents = document.querySelectorAll('.present')
+const boxes = document.querySelectorAll('.box')
+const presentsBox = document.querySelector('.presents-box')
+const chosenBox = document.querySelector('.chosen-box')
+const sendBtn = document.querySelector('.send-btn')
+
+presents.forEach(present => {
+	present.addEventListener('dragstart', () => {
+		present.classList.add('is-dragged')
+	})
+
+	present.addEventListener('dragend', () => {
+		present.classList.remove('is-dragged')
+	})
+})
+
+boxes.forEach(box => {
+	box.addEventListener('dragover', e => {
+		e.preventDefault()
+
+		const isDragged = document.querySelector('.is-dragged')
+		box.appendChild(isDragged)
+
+		notChosen()
+	})
+})
+
+const notChosen = () => {
+	const chosenBoxes = presentsBox.querySelectorAll('.present')
+
+	if (chosenBox.childElementCount > 2) {
+		chosenBoxes.forEach(present => {
+			present.setAttribute('draggable', 'false')
+			present.classList.add('present-disabled')
+		})
+
+		sendBtn.disabled = false
+	} else {
+		chosenBoxes.forEach(present => {
+			present.setAttribute('draggable', 'true')
+			present.classList.remove('present-disabled')
+		})
+
+		sendBtn.disabled = true
+	}
+}
